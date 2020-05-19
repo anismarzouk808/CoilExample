01a828a13b12db0541a76e96ef52d235e51885a2
Anis MARZOUK <anismarzouk808@gmail.com>
2020-05-19 13:50:17 +0200

    wip
    wip

--- app/src/main/java/com/amneils/coilexample/MainActivity.kt
+++ app/src/main/java/com/amneils/coilexample/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.amneils.coilexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.CircularPropagation
+import androidx.appcompat.app.AppCompatActivity
+import coil.api.load
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
 
-  }
+        myImageView.load(distantImageUrl) {
+            placeholder(R.drawable.ic_launcher_background)
+            transformations(CircleCropTransformation())
+        }
+
+
+    }
+
+    companion object {
+        var distantImageUrl = "https://i.ibb.co/RhXYm93/img-to-load.jpg"
+    }
 }
